902a2c21402e7f42dd72d28a650070279cd5222a
murilosolino <murilo.solino@outlook.com>
2025-04-19 20:11:33 -0300

    User insert

--- src/main/java/com/personalproject/springJpaHibernate/resources/UserResource.java
+++ src/main/java/com/personalproject/springJpaHibernate/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.personalproject.springJpaHibernate.entities.User;
 import com.personalproject.springJpaHibernate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long Id){
         User user = service.findById(Id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{Id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
