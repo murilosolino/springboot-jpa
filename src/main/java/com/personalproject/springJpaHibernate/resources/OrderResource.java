c74284df85cd63daf046916354cdb6fa65745dcd
murilosolino <murilo.solino@outlook.com>
2025-04-20 11:07:56 -0300

    Order  insert, update and delete

--- src/main/java/com/personalproject/springJpaHibernate/resources/OrderResource.java
+++ src/main/java/com/personalproject/springJpaHibernate/resources/OrderResource.java
@@ -4,11 +4,10 @@ import com.personalproject.springJpaHibernate.entities.Order;
 import com.personalproject.springJpaHibernate.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +28,26 @@ public class OrderResource {
         Order obj = orderService.findById(Id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<Order> insert(@RequestBody Order obj){
+        orderService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{Id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        orderService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj){
+        orderService.update(id,obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 
 }
--- src/main/java/com/personalproject/springJpaHibernate/services/OrderService.java
+++ src/main/java/com/personalproject/springJpaHibernate/services/OrderService.java
@@ -2,7 +2,11 @@ package com.personalproject.springJpaHibernate.services;
 
 import com.personalproject.springJpaHibernate.entities.Order;
 import com.personalproject.springJpaHibernate.repositories.OrderRepository;
+import com.personalproject.springJpaHibernate.services.exceptions.DataBaseException;
+import com.personalproject.springJpaHibernate.services.exceptions.ResourceNotFundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,36 @@ public class OrderService {
 
     public Order findById(Long Id){
         Optional<Order> obj = orderRepository.findById(Id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFundException(Id));
+    }
+
+    public Order insert(Order obj){
+        return orderRepository.save(obj);
+    }
+
+    public void delete(Long id){
+        try{
+            Optional<Order> o = orderRepository.findById(id);
+            if(o.isEmpty()){
+                throw new ResourceNotFundException(id);
+            }
+            orderRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataBaseException(e.getMessage());
+        }
+    }
+
+    public Order update(Long id, Order obj){
+        try {
+            Order entity = orderRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return orderRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFundException(id);
+        }
+    }
+
+    private void updateData(Order entity, Order obj) {
+        entity.setOrderStatus(obj.getOrderStatus());
     }
 }
